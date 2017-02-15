0d2da4d02a1857e8bc386013492d20a4beef60cc
kobemiller <kobemiller@126.com>
2017-02-15 09:55:06 +0800

    luspm-6

--- linux-unix-system-programming-manual/proc/necho.c
+++ linux-unix-system-programming-manual/proc/necho.c
@@ -2,10 +2,19 @@
 
 int main(int argc, char *argv[])
 {
+    /*
     int j;
 
     for (j = 0; j < argc; j++)
         printf("argv[%d] = %s\n", j, argv[j]);
+    */
+
+    char **p;
+
+    for (p = argv; *p != NULL; p++)
+        puts(*p);
+
+    sleep(30);
 
     exit(EXIT_SUCCESS);
 }
